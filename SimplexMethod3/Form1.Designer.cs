7a46c888a419d4935632952fe588d3b9438525fd
User <User@MSI>
2024-11-05 02:13:25 +0300

    Добавьте файлы проекта.

---
+++ SimplexMethod3/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SimpleksMetod2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.resultsGridView = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.extrComboBox = new System.Windows.Forms.ComboBox();
+            this.functionGridView = new System.Windows.Forms.DataGridView();
+            this.constraintsGridView = new System.Windows.Forms.DataGridView();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nOfVariablesTextBox = new System.Windows.Forms.TextBox();
+            this.nOfContraintsTextBox = new System.Windows.Forms.TextBox();
+            this.XNGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.constraintsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XNGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(720, 357);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(69, 34);
+            this.clearBtn.TabIndex = 24;
+            this.clearBtn.Text = "Очистить";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(12, 357);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(686, 34);
+            this.goBtn.TabIndex = 23;
+            this.goBtn.Text = "поехали";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // resultsGridView
+            // 
+            this.resultsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsGridView.BackgroundColor = System.Drawing.Color.White;
+            this.resultsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsGridView.Location = new System.Drawing.Point(12, 408);
+            this.resultsGridView.Name = "resultsGridView";
+            this.resultsGridView.Size = new System.Drawing.Size(686, 228);
+            this.resultsGridView.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(729, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "min/max";
+            // 
+            // extrComboBox
+            // 
+            this.extrComboBox.AllowDrop = true;
+            this.extrComboBox.FormattingEnabled = true;
+            this.extrComboBox.Items.AddRange(new object[] {
+            "Max",
+            "Min"});
+            this.extrComboBox.Location = new System.Drawing.Point(720, 241);
+            this.extrComboBox.Name = "extrComboBox";
+            this.extrComboBox.Size = new System.Drawing.Size(69, 21);
+            this.extrComboBox.TabIndex = 20;
+            // 
+            // functionGridView
+            // 
+            this.functionGridView.AllowUserToAddRows = false;
+            this.functionGridView.BackgroundColor = System.Drawing.Color.White;
+            this.functionGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.functionGridView.Location = new System.Drawing.Point(12, 186);
+            this.functionGridView.Name = "functionGridView";
+            this.functionGridView.Size = new System.Drawing.Size(686, 62);
+            this.functionGridView.TabIndex = 19;
+            // 
+            // constraintsGridView
+            // 
+            this.constraintsGridView.AllowUserToAddRows = false;
+            this.constraintsGridView.BackgroundColor = System.Drawing.Color.White;
+            this.constraintsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.constraintsGridView.Location = new System.Drawing.Point(12, 62);
+            this.constraintsGridView.Name = "constraintsGridView";
+            this.constraintsGridView.Size = new System.Drawing.Size(777, 123);
+            this.constraintsGridView.TabIndex = 18;
+            this.constraintsGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.constraintsGridView_CellContentClick);
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(361, 9);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(98, 36);
+            this.okBtn.TabIndex = 17;
+            this.okBtn.Text = "Применить";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Переменные";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ограничения";
+            // 
+            // nOfVariablesTextBox
+            // 
+            this.nOfVariablesTextBox.Location = new System.Drawing.Point(261, 18);
+            this.nOfVariablesTextBox.Name = "nOfVariablesTextBox";
+            this.nOfVariablesTextBox.Size = new System.Drawing.Size(34, 20);
+            this.nOfVariablesTextBox.TabIndex = 14;
+            // 
+            // nOfContraintsTextBox
+            // 
+            this.nOfContraintsTextBox.Location = new System.Drawing.Point(87, 18);
+            this.nOfContraintsTextBox.Name = "nOfContraintsTextBox";
+            this.nOfContraintsTextBox.Size = new System.Drawing.Size(34, 20);
+            this.nOfContraintsTextBox.TabIndex = 13;
+            // 
+            // XNGridView
+            // 
+            this.XNGridView.AllowUserToAddRows = false;
+            this.XNGridView.BackgroundColor = System.Drawing.Color.White;
+            this.XNGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.XNGridView.Location = new System.Drawing.Point(12, 249);
+            this.XNGridView.Name = "XNGridView";
+            this.XNGridView.Size = new System.Drawing.Size(686, 62);
+            this.XNGridView.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 698);
+            this.Controls.Add(this.XNGridView);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.resultsGridView);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.extrComboBox);
+            this.Controls.Add(this.functionGridView);
+            this.Controls.Add(this.constraintsGridView);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nOfVariablesTextBox);
+            this.Controls.Add(this.nOfContraintsTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.resultsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.constraintsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XNGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.DataGridView resultsGridView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox extrComboBox;
+        private System.Windows.Forms.DataGridView functionGridView;
+        private System.Windows.Forms.DataGridView constraintsGridView;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nOfVariablesTextBox;
+        private System.Windows.Forms.TextBox nOfContraintsTextBox;
+        private System.Windows.Forms.DataGridView XNGridView;
+    }
+}
+
---
+++ SimplexMethod3/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SimpleksMetod2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int constraintsCount = 0;
+        int variablesCount = 0;
+        int n = 0;
+        int m = 0;
+        double[,] table; //симплекс таблица
+
+
+        List<int> basis; //список базисных переменных
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                constraintsCount = Convert.ToInt32(nOfContraintsTextBox.Text);
+                variablesCount = Convert.ToInt32(nOfVariablesTextBox.Text);
+                fillConstraintsGrid();
+                fillFunctionGrid();
+                XnFunctionGrid();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        void fillConstraintsGrid()
+        {
+            constraintsGridView.Rows.Clear();
+            constraintsGridView.ColumnCount = variablesCount + 2;
+            constraintsGridView.RowHeadersVisible = false;
+            for (int i = 0; i < variablesCount + 2; i++)
+            {
+                constraintsGridView.Columns[i].Width = 50;
+                constraintsGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                if (i < variablesCount)
+                {
+                    constraintsGridView.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+                else if (i == variablesCount)
+                {
+                    constraintsGridView.Columns[i].Name = "Знак";
+                    constraintsGridView.Columns[i+1].Name = "Рез.";
+                }
+
+            }
+
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                string[] row = new string[variablesCount];
+                constraintsGridView.Rows.Add(row);
+                constraintsGridView.Rows[i].Height = 20;
+            }
+        }
+        void fillFunctionGrid()
+        {
+            functionGridView.Rows.Clear();
+            functionGridView.ColumnCount = variablesCount;
+            functionGridView.RowHeadersVisible = false;
+            for (int i = 0; i < variablesCount; i++)
+            {
+                functionGridView.Columns[i].Width = 50;
+                functionGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                if (i < variablesCount)
+                {
+                    functionGridView.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+
+            }
+            functionGridView.Rows.Add();
+
+        }
+        void XnFunctionGrid()
+        {
+            XNGridView.Rows.Clear();
+            XNGridView.ColumnCount = variablesCount;
+            XNGridView.RowHeadersVisible = false;
+            for (int i = 0; i < variablesCount; i++)
+            {
+                XNGridView.Columns[i].Width = 50;
+                XNGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                if (i < variablesCount)
+                {
+                    XNGridView.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+
+            }
+            XNGridView.Rows.Add();
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            nOfContraintsTextBox.Clear();
+            nOfVariablesTextBox.Clear();
+            resultsGridView.Columns.Clear();
+            functionGridView.Columns.Clear();
+            constraintsGridView.Columns.Clear();
+            extrComboBox.SelectedIndex = -1;
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+
+            // Проверка выбора варианта (Max/Min)
+            if (extrComboBox.SelectedItem == null || (extrComboBox.SelectedItem.ToString() != "Max" && extrComboBox.SelectedItem.ToString() != "Min"))
+            {
+                MessageBox.Show("Выберите корректный вариант экстремума (Max или Min).", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Проверка наличия корректных знаков в предпоследнем столбце constraintsGridView
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                string constraintSign = constraintsGridView.Rows[i].Cells[variablesCount].Value?.ToString();
+                if (constraintSign != "<=" && constraintSign != ">=" && constraintSign != "=")
+                {
+                    MessageBox.Show($"Некорректный знак ограничения в строке {i + 1}. Допустимые значения: '<=', '>=', '='.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Проверка наличия чисел в последнем столбце constraintsGridView (свободные члены)
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                if (!double.TryParse(constraintsGridView.Rows[i].Cells[variablesCount + 1].Value?.ToString(), out double _))
+                {
+                    MessageBox.Show($"Некорректное значение свободного члена в строке {i + 1}. Убедитесь, что это число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Проверка наличия чисел в столбцах ограничений constraintsGridView
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                for (int j = 0; j < variablesCount; j++)
+                {
+                    if (!double.TryParse(constraintsGridView.Rows[i].Cells[j].Value?.ToString(), out double _))
+                    {
+                        MessageBox.Show($"Некорректное значение в строке {i + 1}, столбце {j + 1}. Убедитесь, что это число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+
+            // Проверка наличия чисел в столбцах functionGridView (целевой функции)
+            for (int j = 0; j < variablesCount; j++)
+            {
+                if (!double.TryParse(functionGridView.Rows[0].Cells[j].Value?.ToString(), out double _))
+                {
+                    MessageBox.Show($"Некорректное значение в целевой функции, столбец {j + 1}. Убедитесь, что это число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            /*
+            // Проверка наличия корректных знаков в XNGridView (если есть этот элемент)
+            for (int i = 0; i < XNGridView.RowCount; i++)
+            {
+                string constraintSign = XNGridView.Rows[i].Cells[0].Value?.ToString();
+                if (constraintSign != "<=" && constraintSign != ">=" && constraintSign != "=")
+                {
+                    MessageBox.Show($"Некорректный знак в XNGridView в строке {i + 1}. Допустимые значения: '<=', '>=', '='.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }*/
+
+
+
+            Simplex();
+
+            // Инициализация таблицы для результатов
+            resultsGridView.ColumnCount = variablesCount + 1; // Количество столбцов (переменные + свободные члены)
+            resultsGridView.Columns[0].Name = "Переменная";
+            for (int i = 0; i < variablesCount; i++)
+            {
+                resultsGridView.Columns[i + 1].Name = $"X{i + 1}";
+            }
+
+            double[] result = new double[variablesCount];
+            Calculate(result);
+        }
+
+        public void Simplex()
+        {
+            m = constraintsCount + 1; // Количество строк (ограничения + целевая функция)
+            n = variablesCount + constraintsCount + 1; // Количество столбцов (переменные + базисные переменные + свободные члены)
+            table = new double[m, n];
+
+            basis = new List<int>(); // Инициализация списка базисных переменных
+
+            int slackVarIndex = variablesCount + 1;
+
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                // Заполнение коэффициентов переменных
+                for (int j = 0; j < variablesCount; j++)
+                {
+                    table[i, j + 1] = Convert.ToDouble(constraintsGridView.Rows[i].Cells[j].Value);
+                }
+
+                // Заполнение свободного члена
+                table[i, 0] = Convert.ToDouble(constraintsGridView.Rows[i].Cells[variablesCount + 1].Value);
+
+                // Обработка знаков ограничений
+                string constraintSign = constraintsGridView.Rows[i].Cells[variablesCount].Value.ToString();
+                if (constraintSign == "<=")
+                {
+                    table[i, slackVarIndex++] = 1; // Добавление фиктивной переменной
+                }
+                else if (constraintSign == ">=")
+                {
+                    table[i, slackVarIndex++] = -1; // Добавление фиктивной переменной с отрицательным коэффициентом
+                    for (int j = 0; j <= variablesCount; j++) // Инвертирование строки ограничения
+                    {
+                        table[i, j] = -table[i, j];
+                    }
+                }
+                else if (constraintSign == "=")
+                {
+                    // Для знака "=" фиктивная переменная не добавляется
+                }
+
+                // Добавление базисной переменной в список для знаков "<=" и ">="
+                if (constraintSign != "=")
+                {
+                    basis.Add(slackVarIndex - 1);
+                }
+            }
+
+            // Заполнение строки целевой функции
+            string isMaximization = extrComboBox.SelectedItem.ToString();
+            for (int j = 0; j < variablesCount; j++)
+            {
+                if (isMaximization == "Max")
+                {
+                    table[m - 1, j + 1] = -Convert.ToDouble(functionGridView.Rows[0].Cells[j].Value);
+                }
+                else // Минимизация
+                {
+                    table[m - 1, j + 1] = Convert.ToDouble(functionGridView.Rows[0].Cells[j].Value);
+                }
+            }
+        }
+
+
+
+        public double[,] Calculate(double[] result)
+        {
+            int mainCol, mainRow;
+            while (!IsItEnd())
+            {
+                mainCol = findMainCol();
+                mainRow = findMainRow(mainCol);
+
+                if (mainRow == -1)
+                {
+                    MessageBox.Show("Решение неограниченно или система несовместна.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+
+                // убеждаемся, что `basis` содержит достаточно элементов
+                if (mainRow >= basis.Count)
+                {
+                    basis.Add(mainCol);  // Добавляем элемент, если его нет в `basis`
+                }
+                else
+                {
+                    basis[mainRow] = mainCol; // Иначе заменяем элемент
+                }
+
+                double[,] new_table = new double[m, n];
+
+                // Пересчет симплекс-таблицы
+                for (int j = 0; j < n; j++)
+                    new_table[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol];
+
+                for (int i = 0; i < m; i++)
+                {
+                    if (i == mainRow) continue;
+                    for (int j = 0; j < n; j++)
+                        new_table[i, j] = table[i, j] - table[i, mainCol] * new_table[mainRow, j];
+                }
+
+                table = new_table;
+
+                // Отображение текущей симплекс-таблицы после каждой итерации
+                AddTableToResultsGridView();
+            }
+
+            // Заполнение результатов
+            for (int i = 0; i < result.Length; i++)
+            {
+                int k = basis.IndexOf(i + 1);
+                result[i] = (k != -1) ? table[k, 0] : 0;
+            }
+
+            return table;
+        }
+
+        private void AddTableToResultsGridView()
+        {
+            // убеждаемся, что количество столбцов соответствует количеству элементов в симплекс-таблице
+            if (resultsGridView.ColumnCount < n)
+            {
+                resultsGridView.ColumnCount = n;
+            }
+
+            // Добавляем разделяющую строку с количеством пустых ячеек, равным числу столбцов
+            DataGridViewRow separatorRow = new DataGridViewRow();
+            separatorRow.CreateCells(resultsGridView);
+            for (int i = 0; i < resultsGridView.ColumnCount; i++)
+            {
+                separatorRow.Cells[i].Value = "---------";
+            }
+            resultsGridView.Rows.Add(separatorRow);
+
+            // Добавляем текущую симплекс-таблицу в resultsGridView
+            for (int i = 0; i < m; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(resultsGridView);
+
+                for (int j = 0; j < n; j++)
+                {
+                    row.Cells[j].Value = table[i, j];
+                }
+
+                resultsGridView.Rows.Add(row);
+            }
+
+            // Добавляем пустую строку после каждой таблицы для наглядности
+            resultsGridView.Rows.Add(new DataGridViewRow());
+        }
+
+        private bool IsItEnd()
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (table[m - 1, j] < 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private int findMainCol()
+        {
+            int mainCol = 1;
+            for (int j = 2; j < n; j++)
+            {
+                if (table[m - 1, j] < table[m - 1, mainCol]) 
+                    mainCol = j;
+            }
+            return mainCol;
+        }
+
+        private int findMainRow(int mainCol)
+        {
+            int mainRow = -1;
+            for (int i = 0; i < m - 1; i++)
+            {
+                if (table[i, mainCol] > 0)
+                {
+                    if (mainRow == -1 || (table[i, 0] / table[i, mainCol] < table[mainRow, 0] / table[mainRow, mainCol]))
+                    {
+                        mainRow = i;
+                    }
+                }
+            }
+            return mainRow;
+        }
+
+        private void constraintsGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
